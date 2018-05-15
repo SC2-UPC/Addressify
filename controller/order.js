55ae52859efe734e66677855236a4a1bab418dfe
MikelG <mikel.garralaga@estudiant.upc.edu>
2018-05-15 15:43:00 +0200

    Updates order

--- controller/order.js
+++ controller/order.js
@@ -14,11 +14,30 @@ exports.listAllOrders = function (req, res) {
 };
 
 exports.findById = function (req, res) {
-    res.status(200).json("ok");
+    const orderId = req.params.orderId;
+
+    Order.find({ _id: orderId }, function (err, order) {
+        if (err)
+            res.status(500).send({ message: `Internal server error: ${err}` });
+        else
+            res.status(200).json(order);
+    });
 };
 
 exports.newOrder = function (req, res) {
-    res.status(200).json("ok");
+    const newOrder = new Order(req.body);
+
+    newOrder.save(function (err, order) {
+        if (err) {
+            if (err.code == 11000)
+                res.status(409).send({ message: `Object already exists` });
+            else
+                res.status(500).send({ message: `Internal server error: ${err}` });
+        }
+        else {
+            res.status(200).json(order);
+        }
+    });
 };
 
 exports.arrived = function (req, res) {
@@ -30,5 +49,12 @@ exports.delivered = function (req, res) {
 };
 
 exports.delete = function (req, res) {
-    res.status(200).json("ok");
+    const orderId = req.params.orderId;
+
+    Order.findByIdAndRemove(orderId, function (err, order) {
+        if (err)
+            res.status(500).send({ message: `Internal server error: ${err}` });
+        else
+            res.status(200).json({ message: 'Object successfully deleted' });
+    });
 };
--- models/order.js
+++ models/order.js
@@ -4,11 +4,11 @@ var pointModel = require('./point');
 var Point = mongoose.model('Point');
 
 var order = mongoose.Schema({
-    IdO: {type: String},
-    IdPoint: {type:Schema.ObjectId, ref: 'Point'},
-    kPub: { type : String },
-    arrived: {type:Boolean,required : false},
-	delivered: {type:Boolean,required : false}
-},{collection:'orders'});
+    IdO: { type: String, unique: true },
+    IdPoint: { type: Schema.ObjectId, ref: 'Point' },
+    kPub: { type: String },
+    arrived: { type: Boolean, default: false },
+    delivered: { type: Boolean, default: false }
+}, { collection: 'orders' });
 
-module.exports=mongoose.model('Order', order);
+module.exports = mongoose.model('Order', order);
