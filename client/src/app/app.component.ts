b805abfce2efca9bf49c3900f54dd51b19e075b7
MikelG <mikel.garralaga@estudiant.upc.edu>
2018-06-05 18:22:47 +0200

    Add qr generator to web client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {QRCodeComponent} from 'angular2-qrcode';
+
 declare var require: any;
 const rsa = require('../../../../../RSA-js')
 
@@ -9,13 +11,14 @@ const rsa = require('../../../../../RSA-js')
 })
 export class AppComponent {
   title = 'app';
+  d;
 
   ngOnInit() {
     const keys = rsa.getRSAKeys(128);
     const m ="75BCD15"; //123456789
     const c = keys.publicKey.encrypt(m)
-    const d = keys.privateKey.decrypt(c)
-    const t = parseInt(d,16)
-   console.log(t);
+    this.d = keys.privateKey.decrypt(c)
+    const t = parseInt(this.d,16)
+   console.log(this.d);
   }
 }
