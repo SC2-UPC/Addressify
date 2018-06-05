7e397e6443849dd09adf4de6a01051101ce8f939
MikelG <mikel.garralaga@estudiant.upc.edu>
2018-06-05 17:58:18 +0200

    Rsa module working on client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+declare var require: any;
+const rsa = require('../../../../../RSA-js')
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  ngOnInit() {
+    const keys = rsa.getRSAKeys(128);
+    const m ="75BCD15"; //123456789
+    const c = keys.publicKey.encrypt(m)
+    const d = keys.privateKey.decrypt(c)
+    const t = parseInt(d,16)
+   console.log(t);
+  }
 }
